be87067c6c576f941067e9abda338d9c3e0a2db9
John Phillips <johnphillips.dev@gmail.com>
2021-12-01 17:34:27 +0000

    Header added and templated with Nav.

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss'],
+})
+export class HeaderComponent implements OnInit {
+  planets = [
+    'Mercury',
+    'Venus',
+    'Earth',
+    'Mars',
+    'Jupiter',
+    'Saturn',
+    'Uranus',
+    'Neptune',
+  ];
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
