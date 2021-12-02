91ba86a608eaac4c38caf151c88de9a222d6667c
John Phillips <johnphillips.dev@gmail.com>
2021-12-02 14:37:32 +0000

    Reset changes, temporary ignoring router.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { SubNavComponent } from './sub-nav/sub-nav.component';
-import { PlanetsComponent } from './planets/planets.component';
-import { PlanetDetailsComponent } from './planets/planet-details/planet-details.component';
 import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    SubNavComponent,
-    PlanetsComponent,
-    PlanetDetailsComponent,
-  ],
+  declarations: [AppComponent, HeaderComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
