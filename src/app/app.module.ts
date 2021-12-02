0865e2962e4aa112b3f8f076ee0714605f66885d
John Phillips <johnphillips.dev@gmail.com>
2021-12-02 14:10:25 +0000

    Planet components added for setup.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { SubNavComponent } from './sub-nav/sub-nav.component';
+import { PlanetsComponent } from './planets/planets.component';
+import { PlanetDetailsComponent } from './planets/planet-details/planet-details.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent],
+  declarations: [AppComponent, HeaderComponent, SubNavComponent, PlanetsComponent, PlanetDetailsComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
