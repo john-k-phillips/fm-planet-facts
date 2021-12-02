2814e06f8b67142c0863af41f124177fa5a52da5
John Phillips <johnphillips.dev@gmail.com>
2021-12-02 10:30:38 +0000

    Change array to object for colors.

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,14 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   planets = [
-    'Mercury',
-    'Venus',
-    'Earth',
-    'Mars',
-    'Jupiter',
-    'Saturn',
-    'Uranus',
-    'Neptune',
+    { name: 'Mercury', color: '#DEF4FC' },
+    { name: 'Venus', color: '#F7CC7F' },
+    { name: 'Earth', color: '#545BFE' },
+    { name: 'Mars', color: '#FF6A45' },
+    { name: 'Jupiter', color: '#ECAD7A' },
+    { name: 'Saturn', color: '#FCCB6B' },
+    { name: 'Uranus', color: '#65F0D5' },
+    { name: 'Neptune', color: '#497EFA' },
   ];
 
   constructor() {}
