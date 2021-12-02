65cf18b15dcd0c223cf7c171938db3b944b7cb20
John Phillips <johnphillips.dev@gmail.com>
2021-12-02 10:53:05 +0000

    Header correctly styled and toggles with animation

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,6 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  @ViewChild('navContent') navContent: any;
+  @ViewChild('navBtn') navButton: any;
+
+  navIsOpen = false;
   planets = [
     { name: 'Mercury', color: '#DEF4FC' },
     { name: 'Venus', color: '#F7CC7F' },
@@ -20,4 +24,21 @@ export class HeaderComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {}
+
+  onToggleClick() {
+    this.navIsOpen = !this.navIsOpen;
+    if (this.navIsOpen) {
+      this.navContent.nativeElement.style.display = 'block';
+      setTimeout(() => {
+        this.navContent.nativeElement.style.transform = 'translateY(0px)';
+        this.navButton.nativeElement.ariaExpanded = 'true';
+      }, 100);
+    } else {
+      this.navContent.nativeElement.style.transform = 'translateY(-110%)';
+      this.navButton.nativeElement.ariaExpanded = 'false';
+      setTimeout(() => {
+        this.navContent.nativeElement.style.display = 'none';
+      }, 600);
+    }
+  }
 }
